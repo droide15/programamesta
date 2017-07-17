89a88bb4255afc5388f1837c514e8798bbc65615
droide15 <nuniez.jorge@gmail.com>
2017-07-17 01:21:53 -0700

    ya resetea

--- default.aspx.cs
+++ default.aspx.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
 using System.Configuration;
+using System.Collections.Generic;
+using LumenWorks.Framework.IO.Csv;
 using System.Data.Odbc;
 
 public partial class _Default : System.Web.UI.Page
@@ -11,20 +14,45 @@ public partial class _Default : System.Web.UI.Page
             using (OdbcConnection connection = new OdbcConnection(ConfigurationManager.ConnectionStrings["MySQLConnStr"].ConnectionString))
             { // http://asp.net-tutorials.com/mysql/getting-started/
                 connection.Open();
-                /*using (OdbcCommand command = new OdbcCommand("SELECT name FROM test_users", connection))
-                using (OdbcDataReader dr = command.ExecuteReader())
+                
+                OdbcCommand MyCommand =
+                  new OdbcCommand("TRUNCATE TABLE sat_catalogos",
+                                  connection);
+                MyCommand.ExecuteNonQuery();
+
+                FileInfo archivo = new FileInfo(Server.MapPath(".") + @"\sat_catalogos.csv");
+
+                using (CsvReader csv = new CsvReader(new StreamReader(archivo.FullName), true))
                 {
-                    while (dr.Read())
-                        Response.Write(dr["name"].ToString() + "<br />");
-                    dr.Close();
-                }*/
-                Reini_msg.Text = "Connected to Database Server !!";
+                    int fieldCount;
+                    string ms_campos;
+                    string ms_valores;
+                    List<String> valores = new List<string>();
+
+                    fieldCount = csv.FieldCount;
+                    ms_campos = string.Join(",", csv.GetFieldHeaders());
+                    while (csv.ReadNextRecord())
+                    {
+                        if (string.IsNullOrEmpty(csv[0]))
+                            continue;
+                        valores.Clear();
+                        for (int i = 0; i < fieldCount; i++)
+                            valores.Add("\"" + csv[i] + "\"");
+                        ms_valores = string.Join(",", valores.ToArray());
+
+                        MyCommand.CommandText = "INSERT INTO sat_catalogos VALUES(" + ms_valores + ")";
+                        MyCommand.ExecuteNonQuery();
+                    }
+                }
+
                 connection.Close();
+
+                Reini_msg.Text = "La tabla de catalogos ha sido reiniciada a su estado original";
             }
         }
         catch (Exception ex)
         {
             Reini_msg.Text = "An error occured: " + ex.Message;
         }
-	}
+    }
 }
